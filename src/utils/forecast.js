5f2f8e925cb49d7abf4eebb0363d702e1d03135f
CarlosDevBr <cahzerohenrique@gmail.com>
2022-01-13 13:13:13 -0300

    update src/utils/forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 const chalk = require('chalk')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=7b9504fbd97294097995148c63564f89&query=' + latitude +',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=7b9504fbd97294097995148c63564f89&query=' + latitude + ',' + longitude
 
     request({ url , json: true }, (error, {body}) => {   
         if (error){
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(('Unable to find location'), undefined)
         } else {
             const weather = body.current
-            callback(undefined,('It is currently '+ weather.temperature + ' degress out. It feel like '+ weather.feelslike +' degress out and localtime is ' + body.location.localtime))
+            callback(undefined,('It is currently '+ weather.temperature + ' degress out. It feel like '+ weather.feelslike +' degress out. The localtime is ' + body.location.localtime + '. The humidity is ' + weather.humidity + '%.'))
         }
     })
 }
